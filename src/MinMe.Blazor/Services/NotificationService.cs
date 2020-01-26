bd71bfc9792ab6dd8bc378e06f9dba40c3886f6e
Sergey Tihon <sergey.tihon@gmail.com>
2020-01-26 20:53:08 +0300

    Simple Blazor / Electron App

---
+++ src/MinMe.Blazor/Services/NotificationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Blazored.Toast.Services;
+using ElectronNET.API;
+
+namespace MinMe.Blazor.Services
+{
+    public class NotificationService
+    {
+        private readonly IToastService _toastService;
+
+        public NotificationService(IToastService toastService)
+        {
+            _toastService = toastService;
+        }
+
+        public void ShowError(string errorMessage, string title = "MinMe Error")
+        {
+            if (HybridSupport.IsElectronActive)
+            {
+                Electron.Dialog.ShowErrorBox(title, errorMessage);
+            }
+            else
+            {
+                _toastService.ShowError(errorMessage, title);
+            }
+        }
+    }
+}
