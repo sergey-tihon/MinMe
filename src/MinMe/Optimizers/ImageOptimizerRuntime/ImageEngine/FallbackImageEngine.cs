c39411f138019b0b4076d3c2c01cf8ae39dbcd30
Sergey Tihon <sergey.tihon@gmail.com>
2020-04-21 09:35:35 +0300

    Fallback image engine

--- src/MinMe/Optimizers/ImageOptimizerRuntime/ImageEngine/FallbackImageEngine.cs
+++ src/MinMe/Optimizers/ImageOptimizerRuntime/ImageEngine/FallbackImageEngine.cs
@@ -6,16 +6,15 @@ using MinMe.Optimizers.ImageOptimizerRuntime.Model;
 
 namespace MinMe.Optimizers.ImageOptimizerRuntime.ImageEngine
 {
-    internal class ChooseBestImageEngine : IImageEngine
+    internal class FallbackImageEngine : IImageEngine
     {
-        public ChooseBestImageEngine(params IImageEngine[] engines)
+        public FallbackImageEngine(params IImageEngine[] engines)
             => _engines = engines;
 
         private readonly IImageEngine[] _engines;
 
         Stream? IImageEngine.Transform(Stream imageStream, ImageCrop? crop, Size? size)
         {
-            Stream? result = null;
             foreach (var engine in _engines)
             {
                 Stream? stream = null;
@@ -31,17 +30,10 @@ namespace MinMe.Optimizers.ImageOptimizerRuntime.ImageEngine
                 if (stream is null)
                     continue;
 
-                if (result is null || stream.Length < result.Length)
-                {
-                    result?.Dispose();
-                    result = stream;
-                }
-                else
-                {
-                    stream.Dispose();
-                }
+                return stream;
             }
-            return result;
+
+            return null;
         }
     }
 }
