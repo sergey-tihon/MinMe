c7136d33f939f7ec8e26c2e2ee1ce55710d4bf26
Sergey Tihon <sergey.tihon@gmail.com>
2025-11-28 14:09:54 +0100

    feat: migration to .net 10

--- build.fsx
+++ build.fsx
@@ -61,13 +61,13 @@ pipeline "build" {
 
     stage "Publish Windows App" {
         run "dotnet publish src/MinMe.Avalonia -r win-x64 -c Release /p:PublishSingleFile=true"
-        run "cp src/MinMe.Avalonia/bin/Release/net8.0/win-x64/publish/MinMe.Avalonia.exe bin/"
+        run "cp src/MinMe.Avalonia/bin/Release/net10.0/win-x64/publish/MinMe.Avalonia.exe bin/"
     }
 
     stage "Publish macOS App" {
         run "dotnet publish src/MinMe.Avalonia -r osx-x64 -c Release --self-contained -p:UseAppHost=true"
         run "mkdir -p bin/MinMe.app/Contents/MacOS/"
-        run "cp -r src/MinMe.Avalonia/bin/Release/net8.0/osx-x64/publish/ bin/MinMe.app/Contents/MacOS"
+        run "cp -r src/MinMe.Avalonia/bin/Release/net10.0/osx-x64/publish/ bin/MinMe.app/Contents/MacOS"
         run "mkdir -p bin/MinMe.app/Contents/Resources/"
         run "cp src/MinMe.Avalonia/Assets/Info.plist bin/MinMe.app/Contents/"
         run "cp src/MinMe.Avalonia/Assets/AppIcon.icns bin/MinMe.app/Contents/Resources/"
